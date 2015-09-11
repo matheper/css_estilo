894d78760806bc02b12656e348b28bfba28f5f1c
Matheus Pereira <matheper@gmail.com>
2015-09-11 16:54:47 -0300

    Add paginação.

--- static/javascript/javascripts.js
+++ static/javascript/javascripts.js
@@ -4,16 +4,19 @@ $(document).on("click", "#change-color", function(e){
 });
 
 $(document).keydown(function(e){
-    if (e.keyCode == 37) {
-        $('.slide').prev().ScrollTo({
-            duration: 2000,
-            easing: 'linear'
-        });
-    }
-    if (e.keyCode == 39) {
-        $('.slide').next().ScrollTo({
-            duration: 2000,
-            easing: 'linear'
-        });
+    if (e.keyCode == 37 || e.keyCode == 39){
+        var active = $('.slide.active');
+        console.log(active);
+        var slides = $(".slide");
+        var slide_index = slides.index(active);
+        var next_slide;
+        if (e.keyCode == 37) next_slide = $(slides.get(slide_index - 1));
+        if (e.keyCode == 39) next_slide = $(slides.get(slide_index + 1));
+        console.log(next_slide);
+        if (next_slide){
+            active.removeClass('active');
+            next_slide.addClass('active');
+            $("html, body").animate({scrollTop: next_slide.offset().top}, 500);
+        }
     }
 });
